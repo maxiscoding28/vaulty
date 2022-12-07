33c915a0ac41fbe836d8109193554958f4bd700c
max <maxtwinslow1@gmail.com>
2022-12-07 12:49:21 -0700

    provider

--- v/provider/auth_methods/main.tf
+++ v/provider/auth_methods/main.tf
@@ -1,35 +1,32 @@
-# resource "vault_auth_backend" "cert" {
-#     path = "cert"
-#     type = "cert"
-# }
+resource "vault_auth_backend" "userpass" {
+  path = "userpass"
+  type = "userpass"
+}
 
-# resource "vault_auth_backend" "approle" {
-#   path = "approle"
-#   type = "approle"
-# }
+resource "vault_auth_backend" "approle" {
+  path = "approle"
+  type = "approle"
+}
 
-# ## MFA
-# resource "vault_identity_mfa_duo" "example" {
-#   api_hostname    = "api-xxxxxxxx.duosecurity.com"
-#   secret_key      = "secret-key"
-#   integration_key = "secret-int-key"
-# }
+resource "vault_auth_backend" "cert" {
+    path = "cert"
+    type = "cert"
+}
 
-# # Policies
-# resource "vault_policy" "example" {
-#   name = "dev-team"
+## Add Login MFA
 
-#   policy = <<EOT
-# path "secret/my_app" {
-#   capabilities = ["update"]
-# }
-# EOT
-# }
+## Add Github
 
-# data "vault_policy_document" "example" {
-#   rule {
-#     path         = "secret/*"
-#     capabilities = ["create", "read", "update", "delete", "list"]
-#     description  = "allow all on secrets"
-#   }
-# }
+## Add Okta
+
+## Add K*s
+
+## Add JWT
+
+## Add AWS
+
+## Add Azure
+
+## Add Google Cloud
+
+## Add LDAP
